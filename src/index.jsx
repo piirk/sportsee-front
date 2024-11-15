ebe97a7bacda89e26fb08cbd76e18e98ef2142bf
piirk <baptiste.boucharechas@gmail.com>
2024-11-15 17:13:27 +0100

    global skeleton structure and dependencies

---
+++ src/index.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import './index.scss'
+import Dashboard from './pages/Dashboard/Dashboard'
+
+const root = ReactDOM.createRoot(document.getElementById('root'))
+root.render(
+  <React.StrictMode>
+    <Dashboard />
+  </React.StrictMode>,
+)
---
+++ src/pages/Dashboard/Dashboard.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import './Dashboard.scss'
+
+const Dashboard = () => {
+  return (
+    <div className="ss-dashboard">
+      <h1>Dashboard</h1>
+    </div>
+  )
+}
+
+export default Dashboard
