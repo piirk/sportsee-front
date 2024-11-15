f6664ccf1fb6e4affe7081a08bbd1db65b273a97
piirk <baptiste.boucharechas@gmail.com>
2024-11-15 20:08:59 +0100

    Enhance Dashboard with custom greeting and basic html/css layout

--- src/pages/Dashboard/Dashboard.jsx
+++ src/pages/Dashboard/Dashboard.jsx
@@ -13,7 +13,23 @@ const Dashboard = () => {
           </span>
         </footer>
       </div>
-      <h1>Dashboard</h1>
+      <div className="ss-dashboard__content">
+        <section className="ss-dashboard__content__header">
+          <h1>
+            Bonjour{' '}
+            <span className="ss-dashboard__content__header__name">
+              {'Thomas'}
+            </span>
+          </h1>
+          <p>Félicitations ! Vous avez explosé vos objectifs hier 👏</p>
+        </section>
+        <section className="ss-dashboard__main">
+          <div className="ss-dashboard__main__card">
+            <h2>Activité quotidienne</h2>
+          </div>
+          <div className="ss-dashboard__main__card"></div>
+        </section>
+      </div>
     </div>
   )
 }
