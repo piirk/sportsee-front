1aec8240f46ba14c2e29c073f616e2e000411a5b
piirk <baptiste.boucharechas@gmail.com>
2024-11-15 18:19:31 +0100

    Add react-router-dom for routing support and update index.jsx routes

--- src/index.jsx
+++ src/index.jsx
@@ -1,11 +1,21 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 import './index.scss'
 import Dashboard from './pages/Dashboard/Dashboard'
 
 const root = ReactDOM.createRoot(document.getElementById('root'))
 root.render(
   <React.StrictMode>
-    <Dashboard />
+    <Router
+      future={{
+        v7_startTransition: true,
+        v7_relativeSplatPath: true,
+      }}
+    >
+      <Routes>
+        <Route path="/" element={<Dashboard />} />
+      </Routes>
+    </Router>
   </React.StrictMode>,
 )
