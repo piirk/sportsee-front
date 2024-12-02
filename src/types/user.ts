eb6f198c3366b6b46612f12d2546f0cbe68bbd4e
piirk <baptiste.boucharechas@gmail.com>
2024-12-02 18:21:55 +0100

    Add new types for user data and activity, including KeyData and UserActivity

--- src/types/user.ts
+++ src/types/user.ts
@@ -1,11 +1,29 @@
 export type UserInfos = {
   firstName: string
   lastName: string
-  // ++
+}
+
+export type KeyData = {
+  calorieCount: number
+  proteinCount: number
+  carbohydrateCount: number
+  lipidCount: number
 }
 
 export type UserData = {
   data: {
     userInfos: UserInfos
+    keyData: KeyData
   }
+}
+
+type Session = {
+  day: string //type Date?
+  kilogram: number
+  calories: number
+}
+
+export type UserActivity = {
+  userId: number
+  sessions: Session[]
 }
