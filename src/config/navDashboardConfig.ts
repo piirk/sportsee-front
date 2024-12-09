4c4a2a51d19bd31fb3ed5ddc45a499ae39f2796a
piirk <baptiste.boucharechas@gmail.com>
2024-12-09 22:54:05 +0100

    Refactor Dashboard component to import buttonList from navDashboardConfig and remove unused icon imports

---
+++ src/config/navDashboardConfig.ts
@@ -0,0 +1,11 @@
+import icon1 from '../assets/icon1.svg'
+import icon2 from '../assets/icon2.svg'
+import icon3 from '../assets/icon3.svg'
+import icon4 from '../assets/icon4.svg'
+
+export const buttonList = [
+  { icon: icon1, link: '#' },
+  { icon: icon2, link: '#' },
+  { icon: icon3, link: '#' },
+  { icon: icon4, link: '#' },
+]
--- src/pages/Dashboard/Dashboard.tsx
+++ src/pages/Dashboard/Dashboard.tsx
@@ -17,18 +17,7 @@ import {
   getUserActivity,
   getUserSessions,
 } from '../../services/api'
-
-import icon1 from '../../assets/icon1.svg'
-import icon2 from '../../assets/icon2.svg'
-import icon3 from '../../assets/icon3.svg'
-import icon4 from '../../assets/icon4.svg'
-
-const buttonList = [
-  { icon: icon1, link: '#' },
-  { icon: icon2, link: '#' },
-  { icon: icon3, link: '#' },
-  { icon: icon4, link: '#' },
-]
+import { buttonList } from '../../config/navDashboardConfig'
 
 const gridAreaList = [
   { gridArea: '1 / 1 / 2 / 4' },
