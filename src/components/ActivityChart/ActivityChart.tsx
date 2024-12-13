9834b2bea9c6842f5f5460c703db1cdb0b7a49bc
piirk <baptiste.boucharechas@gmail.com>
2024-12-13 19:01:18 +0100

    Implement ActivityChart component with responsive BarChart for daily activity data visualization

--- src/components/ActivityChart/ActivityChart.tsx
+++ src/components/ActivityChart/ActivityChart.tsx
@@ -1,3 +1,13 @@
+import {
+  ResponsiveContainer,
+  Bar,
+  BarChart,
+  CartesianGrid,
+  Legend,
+  Tooltip,
+  XAxis,
+  YAxis,
+} from 'recharts'
 import { UserActivity } from '../../models/UserActivity'
 
 type ActivityChartProps = {
@@ -5,10 +15,47 @@ type ActivityChartProps = {
 }
 
 const ActivityChart: React.FC<ActivityChartProps> = ({ data }) => {
+  const formattedData = data.getData().sessions.map((session) => ({
+    day: new Date(session.day).getDate(),
+    kilogram: session.kilogram,
+    calories: session.calories,
+  }))
+
   return (
-    <div>
-      <h2>Activité quotidienne</h2>
-    </div>
+    <ResponsiveContainer width="100%" height="100%">
+      <BarChart barCategoryGap={60} barGap={8} barSize={7} data={formattedData}>
+        <Legend iconType="circle" verticalAlign="top" />
+        <XAxis dataKey="day" tickSize={20} unit="" tickLine={false} />
+        <YAxis
+          yAxisId="right"
+          orientation="right"
+          domain={['dataMin - 1', 'dataMax + 1']}
+          tickSize={20}
+          dataKey="kilogram"
+          allowDecimals={false}
+          tickLine={false}
+          axisLine={false}
+        />
+        <YAxis yAxisId="left" dataKey="calories" hide={true} />
+        <Tooltip />
+        <CartesianGrid strokeDasharray="2 2" vertical={false} />
+        <Bar
+          yAxisId="right"
+          dataKey="kilogram"
+          fill="steelblue"
+          name="Poids (kg)"
+          unit="kg"
+          radius={[10, 10, 0, 0]}
+        />
+        <Bar
+          yAxisId="left"
+          dataKey="calories"
+          name="Calories brûlées (kCal)"
+          unit="kCal"
+          radius={[10, 10, 0, 0]}
+        />
+      </BarChart>
+    </ResponsiveContainer>
   )
 }
 
