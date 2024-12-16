9056b42d6e2435749d5e778a27d053ffb01620b4
piirk <baptiste.boucharechas@gmail.com>
2024-12-16 18:08:39 +0100

    Add custom legend to ActivityChart and style with SCSS

--- src/components/ActivityChart/ActivityChart.tsx
+++ src/components/ActivityChart/ActivityChart.tsx
@@ -9,12 +9,37 @@ import {
   YAxis,
 } from 'recharts'
 import { UserActivity } from '../../models/UserActivity'
-import { useEffect, useState } from 'react'
 import { getUserActivity } from '../../services/api'
 import Error from '../Error/Error'
+import { useEffect, useState } from 'react'
+import './ActivityChart.scss'
 
 type ActivityChartProps = {
   userId: number
+}
+
+const CustomLegend = () => {
+  return (
+    <div className="custom-legend">
+      <span className="custom-legend__title">Activité quotidienne</span>
+      <div className="custom-legend__items">
+        <span className="custom-legend__item">
+          <span
+            className="custom-legend__item--color"
+            style={{ backgroundColor: '#282D30' }}
+          ></span>
+          Poids (kg)
+        </span>
+        <span className="custom-legend__item">
+          <span
+            className="custom-legend__item--color"
+            style={{ backgroundColor: '#E60000' }}
+          ></span>
+          Calories brûlées (kCal)
+        </span>
+      </div>
+    </div>
+  )
 }
 
 const ActivityChart: React.FC<ActivityChartProps> = ({ userId }) => {
@@ -49,40 +74,48 @@ const ActivityChart: React.FC<ActivityChartProps> = ({ userId }) => {
   }))
 
   return (
-    <ResponsiveContainer width="100%" height="100%">
-      <BarChart barCategoryGap={60} barGap={8} barSize={7} data={formattedData}>
-        <Legend iconType="circle" verticalAlign="top" />
-        <XAxis dataKey="day" tickSize={20} unit="" tickLine={false} />
-        <YAxis
-          yAxisId="right"
-          orientation="right"
-          domain={['dataMin - 1', 'dataMax + 1']}
-          tickSize={20}
-          dataKey="kilogram"
-          allowDecimals={false}
-          tickLine={false}
-          axisLine={false}
-        />
-        <YAxis yAxisId="left" dataKey="calories" hide={true} />
-        <Tooltip />
-        <CartesianGrid strokeDasharray="2 2" vertical={false} />
-        <Bar
-          yAxisId="right"
-          dataKey="kilogram"
-          fill="steelblue"
-          name="Poids (kg)"
-          unit="kg"
-          radius={[10, 10, 0, 0]}
-        />
-        <Bar
-          yAxisId="left"
-          dataKey="calories"
-          name="Calories brûlées (kCal)"
-          unit="kCal"
-          radius={[10, 10, 0, 0]}
-        />
-      </BarChart>
-    </ResponsiveContainer>
+    <>
+      <CustomLegend />
+      <ResponsiveContainer width="100%" height="100%">
+        <BarChart
+          barCategoryGap={60}
+          barGap={8}
+          barSize={7}
+          data={formattedData}
+        >
+          <XAxis dataKey="day" tickSize={20} unit="" tickLine={false} />
+          <YAxis
+            yAxisId="right"
+            orientation="right"
+            domain={['dataMin - 1', 'dataMax + 1']}
+            tickSize={20}
+            dataKey="kilogram"
+            allowDecimals={false}
+            tickLine={false}
+            axisLine={false}
+          />
+          <YAxis yAxisId="left" dataKey="calories" hide={true} />
+          <Tooltip />
+          <CartesianGrid strokeDasharray="2 2" vertical={false} />
+          <Bar
+            yAxisId="right"
+            dataKey="kilogram"
+            name="Poids (kg)"
+            unit="kg"
+            fill="#282D30"
+            radius={[10, 10, 0, 0]}
+          />
+          <Bar
+            yAxisId="left"
+            dataKey="calories"
+            name="Calories brûlées (kCal)"
+            unit="kCal"
+            fill="#E60000"
+            radius={[10, 10, 0, 0]}
+          />
+        </BarChart>
+      </ResponsiveContainer>
+    </>
   )
 }
 
