d4f451230448db3828755df2bb84e824c53e8701
piirk <baptiste.boucharechas@gmail.com>
2024-12-17 22:26:50 +0100

    Enhance ScoreChart layout by adding title and adjusting inner/outer radius for improved visual clarity

--- src/components/ScoreChart/ScoreChart.tsx
+++ src/components/ScoreChart/ScoreChart.tsx
@@ -8,33 +8,41 @@ type ScoreChartProps = {
 const ScoreChart: React.FC<ScoreChartProps> = ({ score }) => {
   return (
     <div className="ss-radial-bar-chart">
-      {/* Cercle blanc central */}
-      <div className="ss-radial-bar-chart__circle">
-        {/* Texte centré */}
-        <div className="ss-radial-bar-chart__circle__text">
-          <p>
-            <span className="ss-radial-bar-chart__circle__text__value">
-              {score * 100}%
-            </span>
-            <br />
-            de votre objectif
-          </p>
-        </div>
+      <h2 className="ss-radial-bar-chart__title">Score</h2>
+      <div className="ss-radial-bar-chart__text">
+        <p>
+          <span className="ss-radial-bar-chart__text__value">
+            {score * 100}%
+          </span>
+          <br />
+          de votre objectif
+        </p>
       </div>
 
-      {/* Graphique RadialBar au-dessus du cercle blanc */}
       <ResponsiveContainer width="100%" height="100%">
         <RadialBarChart
           cx="50%"
           cy="50%"
-          innerRadius="100%"
-          outerRadius="100%"
-          barSize={10}
+          innerRadius="75%"
+          outerRadius="75%"
+          barSize={100}
           startAngle={180}
-          endAngle={180 - 360 * score}
+          endAngle={-180}
           data={score ? [{ value: score }] : []}
+          margin={{ top: 0, right: 0, left: 0, bottom: 0 }}
         >
-          <RadialBar cornerRadius={20} dataKey="value" fill="#ff0000" />
+          <RadialBar
+            dataKey="value"
+            fill="#ffffff"
+            data={[{ value: 1 }]}
+            isAnimationActive={false}
+          />
+          <RadialBar
+            maxBarSize={20}
+            cornerRadius={20}
+            dataKey="value"
+            fill="#ff0000"
+          />
         </RadialBarChart>
       </ResponsiveContainer>
     </div>
